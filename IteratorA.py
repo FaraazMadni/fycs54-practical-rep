055fb92a1f3c112869340fbb7806ee4cba5c97d4
FaraazMadni <62214184+FaraazMadni@users.noreply.github.com>
2020-03-16 13:05:41 +0530

    Update IteratorA.py

--- IteratorA.py
+++ IteratorA.py
@@ -1,5 +1,7 @@
-lst1=["Mumbai","Pune","Delhi","Patna"]
-iterator_obj=iter(lst1)
+list1=["Mumbai","Pune","Delhi","Patna"]
+
+iterator_obj=iter(list1)
+
 print(next(iterator_obj))
 print(next(iterator_obj))
 print(next(iterator_obj))
