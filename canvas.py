38070c1eca67044106f89cab1f8d68c8f18c584a
FaraazMadni <62214184+FaraazMadni@users.noreply.github.com>
2020-03-16 13:42:51 +0530

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -1,13 +1,20 @@
+#Canvas Demo
 from tkinter import *
 root=Tk()
 
 canvas_width=500
 canvas_height=500
+
 c=Canvas(root,width=canvas_width,height=canvas_height,bg="green")
 c.pack()
-y=int(canvas_height/2)
+
+y=int(canvas_height/2)    #line coordinate.
+
 c.create_line(10,y,canvas_width,y,fill="blue")
 coord=10,50,240,210
+
 arc=c.create_arc(coord,start=90,extent=190,fill="red")
+
 oval=c.create_oval(50,60,100,100)
+
 root.mainloop()
